4fe05738e9cb3195f52924f6a897f4847009cc66
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-07-22 22:02:59 +0530

    Modify src/components/BookItems.jsx

--- src/components/BookItems.jsx
+++ src/components/BookItems.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
-import '../styles/BookItems.css';
+import PropTypes from 'prop-types';
 
-const BookItems = () => (
+const BookItems = ({ book }) => (
   <div className="bookItemContainer">
     <div className="titleBtns">
       <div className="bookItemTitle">
         <p>Category</p>
-        <h2>Book Title</h2>
-        <p>Author</p>
+        <h2>{book.title}</h2>
+        <p>{book.author}</p>
       </div>
       <div className="btnsContainer">
         <button type="button">Comment</button>
@@ -18,7 +18,7 @@ const BookItems = () => (
       </div>
     </div>
     <div className="statusContainer">
-      <FontAwesomeIcon icon={faCircleNotch} style={{ color: '#013ca2' }} />
+      <FontAwesomeIcon icon={faCircleNotch} style={{ color: '#013CA2' }} />
       <div className="percentage">
         <p>65%</p>
         <p>Completed</p>
@@ -31,5 +31,12 @@ const BookItems = () => (
     </div>
   </div>
 );
+
+BookItems.propTypes = {
+  book: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default BookItems;
