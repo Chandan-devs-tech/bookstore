21cbc0f9916155afeade682f8c9c5000ae7c2840
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-07-28 15:15:01 +0530

    Modify src/redux/categories/categoriesSlice.js

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -7,7 +7,11 @@ const categoriesSlice = createSlice({
   name: 'categories',
   initialState,
   reducers: {
-    checkStatus: () => 'Under construction',
+    checkStatus: (state, action) => {
+      state.categories = action.payload === 'Under construction'
+        ? 'Under construction'
+        : state.categories;
+    },
   },
 });
 
