c5b154f26f72e49783ef41b27cbe14147fd8e623
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-07-28 14:32:00 +0530

    Add  src/redux

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  categories: [],
+};
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {
+    checkStatus: () => 'Under construction',
+  },
+});
+
+export const { checkStatus } = categoriesSlice.actions;
+export default categoriesSlice.reducer;
