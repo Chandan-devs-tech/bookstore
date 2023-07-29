659ab9a2dc4d139a047c23b04cb534b48cbc5490
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-07-29 13:43:36 +0530

    Modify src/components/Books.jsx

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import BookItems from './BookItems';
+// import { v4 as uuidv4 } from 'uuid';
+// import BookItems from './BookItems';
 import Input from './Input';
+import BookList from './BookList';
 import '../styles/BookItems.css';
 
-const Books = () => {
-  // Assuming you have an array of book data called "booksData"
-  const booksData = [
-    { id: uuidv4(), title: 'Book 1', author: 'Author 1' },
-    { id: uuidv4(), title: 'Book 2', author: 'Author 2' },
-    { id: uuidv4(), title: 'Book 3', author: 'Author 3' },
-  ];
-
-  return (
-    <div>
-      {booksData.map((book) => (
-        <BookItems key={book.id} book={book} />
-      ))}
-      <Input />
-    </div>
-  );
-};
-
+const Books = () => (
+  <div>
+    <BookList />
+    <Input />
+  </div>
+);
 export default Books;
