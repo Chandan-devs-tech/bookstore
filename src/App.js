7a1a40a1d6addc204ce4437ea83d5d689b273ad2
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-07-29 13:42:27 +0530

    Modify src/App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Books from './components/Books';
 import Categories from './components/Categories';
+import './App.css';
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
         <Route path="/" element={<Books />} />
         <Route path="/categories" element={<Categories />} />
       </Routes>
+      {/* <Books /> */}
     </div>
   );
 }
