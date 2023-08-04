60bbca37b798008d10adefcaafa4fcdd17362e69
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-08-04 16:38:54 +0530

    Modify  src/components/BookList.jsx

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getBooks } from '../redux/books/bookSlice';
 import BookItems from './BookItems';
 
 function BookList() {
   const booksData = useSelector((state) => state.book.books);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
   return (
     <div>
-      {booksData.map((book) => (
-        <BookItems key={book.id} book={book} />
-      ))}
+      {booksData ? booksData.map((item) => (
+        <BookItems
+          title={item.title}
+          key={item.item_id}
+          id={item.item_id}
+          author={item.author}
+          category={item.category}
+        />
+      )) : (
+        <div>No books yet</div>
+      )}
     </div>
   );
 }
