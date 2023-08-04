feae65f4cb863220f66b7a064974c4122ba6a685
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-08-05 01:01:02 +0530

    Modify src/components/Input.jsx

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { postBook } from '../redux/books/bookSlice';
+import '../styles/Input.css';
 
 const Input = () => {
   const [title, setTitle] = useState('');
@@ -23,30 +24,32 @@ const Input = () => {
     setAuthor(''); // Clear the author input
   };
   return (
-    <div>
-      <h2>Add New Book</h2>
-      <form>
+    <div className="formContainer">
+      <h2 className="formTitle">Add New Book</h2>
+      <form className="form">
         <input
           type="text"
+          className="titleInput"
           placeholder="Book title"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
         <input
           type="text"
+          className="authorInput"
           placeholder="Author"
           value={author}
           onChange={(e) => setAuthor(e.target.value)}
         />
-        <select value={category} onChange={(e) => setCategory(e.target.value)}>
-          <option value="" disabled selected>Select an option</option>
+        <select className="categories" value={category} onChange={(e) => setCategory(e.target.value)}>
+          <option value="" disabled>Select an option</option>
           <option value="Fiction">Fiction</option>
           <option value="Poetry">Poetry</option>
           <option value="Science and Nature">Science and Nature</option>
           <option value="Mystery/Thriller">Mystery/Thriller</option>
           <option value="Fantasy">Fantasy</option>
         </select>
-        <button type="button" onClick={handleSubmit}>Add Book</button>
+        <button type="button" className="addBtn" onClick={handleSubmit}>Add Book</button>
       </form>
     </div>
   );
